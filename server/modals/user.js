1182a44de96102325ad904abe702ec1a13bf55b1
Hamza <hamzadaoues1@gmail.com>
2019-04-11 22:25:41 +0100

    registration

--- server/modals/user.js
+++ server/modals/user.js
@@ -1,19 +1,58 @@
- var db = require("../helpers/db-setup");
-
+var userID=0;
 // add user to db
-exports.create = function(username, email, done) {
-  var values = [username, email /*, new Date().toISOString()*/];
+exports.create = function(user, done) {
+  var userID=0;
+  console.log("at create") ;
+  console.log(user) ;
+  var values = [user.username, user.mail ,user.password,user.tel];
+ 
   db.get().query(
-    "INSERT INTO Users (username, email) VALUES(?, ?)",
+    "INSERT INTO user (name, email,password,tel) VALUES(?, ?,?,?)",
     values,
     function(err, result) {
       if (err) {
         console.log(err);
         return done(err);
       }
-      done(null, result.insertId);
+     // done(null, result.insertId);
+       userID=result.insertId;
+      console.log(result.insertId);
+    // to check wheather the account type is client or agency 
+console.log(user.account_type)
+if (user.account_type=='client')
+{
+  console.log(userID);
+  db.get().query(
+    "INSERT INTO client (CIN, userid) VALUES(?, ?)",
+  [user.cin,userID],
+    function(err, result) {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      done(null, userID);
     }
   );
+}
+else{
+db.get().query(
+    "INSERT INTO agence (num_patente, description ,iduser) VALUES(?, ?,?)",
+  [user.patente_num,user.about,userID],
+    function(err, result) {
+      if (err) {
+        console.log(err);
+        return done(err);
+      }
+      done(null, userID);
+    }
+  );
+}
+
+    }
+  );
+
+
+
 };
 
 // list all users
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -13,15 +13,14 @@ router.get("/test", (req, res) => res.json({ msg: "test user works fine!" }));
 // @desc    register user
 // @access  Public
 router.get("/register", (req, res) => {
-  userModal.findByEmail(req.body.email, function(err, rows) {
+  userModal.findByEmail(req.query.mail, function(err, rows) {
     if (rows !== undefined && rows.length > 0) {
       return res
         .status(400)
         .json({ email: `email "${req.body.email}" already exist` });
     } else {
       const newUser = userModal.create(
-        req.body.username,
-        req.body.email,
+        req.query,
         function(err, insertId) {
           res.json({ msg: `Succeed! user added with id# ${insertId}` });
         }
